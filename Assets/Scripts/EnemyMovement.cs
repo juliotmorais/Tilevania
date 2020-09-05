b1b7f5a2bf57e1b36741a01597b7254dc31da960
juliotmorais <juliotmorais@gmail.com>
2020-09-05 12:39:26 +0900

    got rid of unused assets, sliced up enemy sprites, added in enemy and added horizontal movement and collision

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Rigidbody2D myRigidBody2D;
+    [SerializeField] float moveSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidBody2D.velocity = new Vector2(moveSpeed, 0f);
+    }
+}
